d01c7a8980803e5008051fe3f2c57212585ff4eb
jpacil0 <joshuaacilo.13+github@gmail.com>
2023-07-10 16:43:29 +0800

    add cp

---
+++ scripts/make_fake_subs.py
@@ -0,0 +1,34 @@
+import random
+import pandas as pd
+from omegaconf import OmegaConf
+from faker import Faker
+fake = Faker()
+
+PATH_CONFIG = "conf/config.yaml"
+cfg = OmegaConf.load(PATH_CONFIG)
+num_subs = cfg.fake_data.num_subs
+min_age = cfg.fake_data.min_age
+max_age = cfg.fake_data.max_age
+
+random.seed(2023)
+
+fake_data = pd.DataFrame()
+
+for i in range(num_subs):
+    uid = f"glo-{i}"
+    gender = random.choice(["male", "female"])
+    name = fake.name_male() if gender=="male" else fake.name_female()
+    age = random.randint(min_age, max_age)
+    chi_indicator = random.choice([0, 1])
+    gcash_user_indicator = random.choice(["Y", "N"])
+    data = pd.DataFrame(
+        dict(
+            uid=uid,
+            gender=gender,
+            age=age,
+            name=name,
+            chi_indicator=chi_indicator,
+            gcash_user_indicator=gcash_user_indicator
+        ), index=[0]
+    )
+    fake_data = pd.concat([fake_data, data], ignore_index=True)
