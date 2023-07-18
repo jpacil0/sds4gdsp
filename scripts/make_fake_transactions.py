021908068aeb2c0c3662551edf3b2c51d68edec4
jpacil0 <joshuaacilo.13+github@gmail.com>
2023-07-18 10:32:02 +0800

    add dist mat

--- scripts/make_fake_transactions.py
+++ scripts/make_fake_transactions.py
@@ -9,7 +9,9 @@ print(f"working @: {curr_dir}")
 
 import random
 import pandas as pd
+from itertools import combinations
 from omegaconf import OmegaConf
+from sds4gdsp.processor import calc_haversine_distance
 
 PATH_CONFIG = "conf/config.yaml"
 cfg = OmegaConf.load(PATH_CONFIG)
@@ -23,6 +25,21 @@ filepath_subscribers = "data/fake_subscribers.csv"
 fake_subscribers = pd.read_csv(filepath_subscribers)
 filepath_cellsites = "data/fake_cellsites.csv"
 fake_cellsites = pd.read_csv(filepath_cellsites)
+
+# make a distance matrix as reference, here are the assumptions:
+# 1: the hop probability is a function of distance
+# 2: the sub is always moving? check this
+cellsite_pairs = list(combinations(fake_cellsites.uid.tolist(), 2))
+dist_matrix = pd.DataFrame(cellsite_pairs, columns=["site1", "site2"])
+dist_matrix = dist_matrix.merge(fake_cellsites, left_on="site1", right_on="uid")\
+    .rename(columns={"coords": "coords1"})\
+    .drop(columns=["uid"])
+dist_matrix = dist_matrix.merge(fake_cellsites, left_on="site2", right_on="uid")\
+    .rename(columns={"coords": "coords2"})\
+    .drop(columns=["uid"])
+dist_matrix["distance"] = dist_matrix.apply(lambda x: calc_haversine_distance(x.coords1, x.coords2), axis=1)
+dist_matrix.sort_values(by=["site1", "distance"], ascending=[0, 0], inplace=True)
+
 
 # save file to local disk
 # fake_transactions = ...
