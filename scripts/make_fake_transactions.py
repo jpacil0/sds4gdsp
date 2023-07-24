e09b54c97813466c982dca9c21387728905449d2
jpacilo <24766217+jpacilo@users.noreply.github.com>
2023-07-24 14:10:14 +0800

    add cp k nearest neighbor

--- scripts/make_fake_transactions.py
+++ scripts/make_fake_transactions.py
@@ -18,6 +18,7 @@ from sds4gdsp.processor import calc_haversine_distance, convert_distances_to_pro
 PATH_CONFIG = "conf/config.yaml"
 cfg = OmegaConf.load(PATH_CONFIG)
 seed = cfg.seed
+k_nearest_neighbor = cfg.fake_transactions.k_nearest_neighbor
 
 # for reproducibility
 random.seed(seed)
@@ -31,6 +32,7 @@ fake_cellsites = pd.read_csv(filepath_cellsites)
 # make a distance matrix as reference, here are the assumptions:
 # 1: the hop probability is a function of distance
 # 2: the sub is always moving? check this
+
 cellsite_pairs = list(permutations(fake_cellsites.uid.tolist(), 2))
 matrix = pd.DataFrame(cellsite_pairs, columns=["site1", "site2"])
 matrix = matrix.merge(fake_cellsites, left_on="site1", right_on="uid")\
@@ -39,86 +41,19 @@ matrix = matrix.merge(fake_cellsites, left_on="site1", right_on="uid")\
 matrix = matrix.merge(fake_cellsites, left_on="site2", right_on="uid")\
     .rename(columns={"coords": "coords2"})\
     .drop(columns=["uid"])
+
+# filter top k possible sites-to-hop per site  
 matrix["distance"] = matrix.apply(lambda x: calc_haversine_distance(x.coords1, x.coords2), axis=1)
-matrix = matrix.sort_values(
+matrix["rank_nearest"] = matrix.groupby("site1")["distance"].rank("min")
+filter_top_k = matrix.rank_nearest <= k_nearest_neighbor
+matrix = matrix.loc[filter_top_k].reset_index(drop=True)
+
+# for sanity checking
+num_rows_per_site = 3
+matrix.sort_values(
     by=["site1", "site2"],
     key=lambda col: col.apply(lambda x: int(x.split("-")[-1])),
-    ascending=[True, True]
+    ascending=[1, 1]
 )\
-    .reset_index(drop=True)
-scaler = StandardScaler()
-# distances = matrix.distance.tolist()
-# probas = convert_distances_to_probas(distances, scaler)
-matrix["distance_scaled"] = matrix.groupby("site1")["distance"].apply(lambda x: scale_feature(x, scaler)).explode().tolist()
-matrix["proba"] = matrix.groupby("site1")["distance_scaled"].apply(lambda x: convert_distances_to_probas(x)).explode().tolist()
-
-# check this out
-matrix.groupby("site1").proba.sum()
-
-f = matrix.site1=="glo-cel-100" 
-matrix.loc[f]\
-    .proba.hist()
-
-
-matrix["scaled_distance"] = matrix.groupby("site1")["distance"].apply(lambda x: scale_feature(x, scaler)).explode().tolist()
-convert_distances_to_probas(col, scaler)
-
-type(apply_softmax(ds))
-type(convert_distances_to_probas(ds, scaler))
-
-
-matrix.groupby("site1")["distance"].apply(lambda col: convert_distances_to_probas(col, scaler)).reset_index()
-
-
-
-
-matrix["latch_proba"] = matrix.groupby("site1")["distance_scaled"].apply(lambda arr: apply_softmax(arr)).tolist()
-
-# check this out, this should be 111
-matrix.groupby("site1").latch_proba.sum().sum()
-
-# next steps
-# 1. try binning the feature
-# 1. standardize or scale; change to minmax scaling
-# 2. invert the order? sub 1
-# 2. apply softmax
-
-
-site_filter = matrix.site1=="glo-cel-10"
-matrix_sample = matrix.loc[site_filter]
-
-
-# standardscaler -> softmax
-
-from sklearn.preprocessing import StandardScaler
-
-arr = [1, 2, 3]
-scaler.fit_transform(np.array(arr).reshape(-1, 1)).flatten().tolist()
-
-random.choices?
-
-# save file to local disk
-# fake_transactions = ...
-# filepath = "data/fake_subscribers.csv"
-# fake_transactions.to_csv(filepath, index=False)
-
-
-ds = matrix_sample.distance.tolist()
-
-scaler = StandardScaler()
-ds_scld = scaler.fit_transform(np.array(ds).reshape(-1, 1)).flatten().tolist()
-
-alpha = 1
-ds_proba = np.exp(-alpha*np.array(ds_scld))
-prob_df = pd.DataFrame(
-    dict(distance=ds, proba=ds_proba)
-)
-display(prob_df.sort_values(by="distance"))
-
-sampleList = [10, 20, 30, 40]
-weights = [0.1, 0.2, 0.6, 0.5]
-random.choices(
-    population=sampleList, weights=weights, k=1
-)
-
-type(convert_distances_to_probas(ds, scaler, 1))
+    .reset_index(drop=True)\
+    .head(k_nearest_neighbor*num_rows_per_site)
