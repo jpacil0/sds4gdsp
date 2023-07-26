6bb20bdfebae09ce7e5406691cfd4a1e05dc4aa2
jpacilo <24766217+jpacilo@users.noreply.github.com>
2023-07-26 18:23:09 +0800

    refactor make fake sub

--- scripts/make_fake_subscribers.py
+++ scripts/make_fake_subscribers.py
@@ -8,42 +8,50 @@ OUTPUT: 'data/fake_subscribers.csv'
 # curr_dir = os.getcwd()
 # print(f"working @: {curr_dir}")
 
+import hydra
 import random
 import pandas as pd
-from omegaconf import OmegaConf
+from omegaconf import DictConfig
 from faker import Faker
-fake = Faker()
 
-PATH_CONFIG = "conf/config.yaml"
-cfg = OmegaConf.load(PATH_CONFIG)
-seed = cfg.seed
-num_subs = cfg.fake_subscribers.num_subs
-min_age = cfg.fake_subscribers.min_age
-max_age = cfg.fake_subscribers.max_age
+@hydra.main(version_base=None, config_path="../conf", config_name="config")
+def main(cfg: DictConfig) -> None:
 
-# for reproducibility
-random.seed(seed)
+    seed = cfg.seed
+    filepath = cfg.fake_subscribers.filepath
+    num_subs = cfg.fake_subscribers.num_subs
+    min_age = cfg.fake_subscribers.min_age
+    max_age = cfg.fake_subscribers.max_age
 
-fake_subscribers = pd.DataFrame()
+    # initialize faker module
+    fake = Faker()
 
-for i in range(num_subs):
-    uid = f"glo-sub-{str(i+1).zfill(3)}"
-    gender = random.choice(["male", "female"])
-    name = fake.name_male() if gender=="male" else fake.name_female()
-    age = random.randint(min_age, max_age)
-    chi_indicator = random.choice([0, 1])
-    gcash_user_indicator = random.choice(["Y", "N"])
-    data = pd.DataFrame(
-        dict(
-            uid=uid,
-            gender=gender,
-            age=age,
-            name=name,
-            chi_indicator=chi_indicator,
-            gcash_user_indicator=gcash_user_indicator
-        ), index=[0]
-    )
-    fake_subscribers = pd.concat([fake_subscribers, data], ignore_index=True)
+    # for reproducibility
+    random.seed(seed)
 
-filepath = "data/fake_subscribers.csv"
-fake_subscribers.to_csv(filepath, index=False)
+    fake_subscribers = pd.DataFrame()
+
+    for i in range(num_subs):
+        uid = f"glo-sub-{str(i+1).zfill(3)}"
+        gender = random.choice(["male", "female"])
+        name = fake.name_male() if gender=="male" else fake.name_female()
+        age = random.randint(min_age, max_age)
+        chi_indicator = random.choice([0, 1])
+        ewallet_user_indicator = random.choice(["Y", "N"])
+        data = pd.DataFrame(
+            dict(
+                uid=uid,
+                gender=gender,
+                age=age,
+                name=name,
+                chi_indicator=chi_indicator,
+                ewallet_user_indicator=ewallet_user_indicator
+            ), index=[0]
+        )
+        fake_subscribers = pd.concat([fake_subscribers, data], ignore_index=True)
+
+    fake_subscribers.to_csv(filepath, index=False)
+    print(f"OK. Successfully saved '{filepath}'")
+
+if __name__ == "__main__":
+    main()
