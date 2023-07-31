773d51a54c984299b2ac6c8ddfa566bc3be9c6a2
jpacilo <24766217+jpacilo@users.noreply.github.com>
2023-07-31 21:30:37 +0800

    add cp mobility idx 1

--- sds4gdsp/plotter.py
+++ sds4gdsp/plotter.py
@@ -37,29 +37,7 @@ def plot_images(imgs, nrows=5, ncols=6, figsize=(10, 10)):
     plt.show()
     return fig
 
-def visualize_route(traj):
-    coords = traj.coords.tolist()
-    od_pairs = list(zip(coords, coords[1:]))
-    routes = []
-    for od_pair in od_pairs:
-        routes.append(
-            shapely.geometry.LineString(
-                map(shapely.wkt.loads, od_pair)
-            )
-        )
-    fig, ax = plt.subplots(1, figsize=(7, 5))
-    gpd.GeoSeries(traj["coords"].apply(shapely.wkt.loads)).plot(ax=ax, color="blue", markersize=60, zorder=2);
-    gpd.GeoSeries(traj.head(1)["coords"].apply(shapely.wkt.loads)).plot(ax=ax, color="green", markersize=120, zorder=3);
-    gpd.GeoSeries(traj.tail(1)["coords"].apply(shapely.wkt.loads)).plot(ax=ax, color="red", markersize=120, zorder=3);
-    gpd.GeoSeries(routes).plot(ax=ax, color="grey", linewidth=3, zorder=1, alpha=0.6)
-    ax.ticklabel_format(useOffset=False)
-    ax.legend(["_", "orig", "dest"])
-    plt.tight_layout()
-    plt.axis("off")
-    plt.close()
-    return fig
-
-def viz_total_travel_distance(sample_traj_low, sample_traj_mid, sample_traj_high):
+def plot_total_travel_distance(sample_traj_low, sample_traj_mid, sample_traj_high):
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(13, 5))
     sample_low = fetch_total_travel_distance(sample_traj_low)
     sample_mid = fetch_total_travel_distance(sample_traj_mid)
@@ -81,6 +59,52 @@ def viz_total_travel_distance(sample_traj_low, sample_traj_mid, sample_traj_high
     ax2.legend(["low", "mid", "high"], frameon=False)
     plt.tight_layout()
     plt.close();
+    return fig
+
+def plot_routes(traj_low, traj_mid, traj_high):
+    coords_low = traj_low.coords.tolist()
+    coords_mid = traj_mid.coords.tolist()
+    coords_high = traj_high.coords.tolist()
+    od_pairs_low = list(zip(coords_low, coords_low[1:]))
+    od_pairs_mid = list(zip(coords_mid, coords_mid[1:]))
+    od_pairs_high = list(zip(coords_high, coords_high[1:]))
+    routes_low, routes_mid, routes_high = [], [], []
+    for od_pair in od_pairs_low:
+        routes_low.append(
+            shapely.geometry.LineString(
+                map(shapely.wkt.loads, od_pair)
+            )
+        )
+    for od_pair in od_pairs_mid:
+        routes_mid.append(
+            shapely.geometry.LineString(
+                map(shapely.wkt.loads, od_pair)
+            )
+        )
+    for od_pair in od_pairs_high:
+        routes_high.append(
+            shapely.geometry.LineString(
+                map(shapely.wkt.loads, od_pair)
+            )
+        )
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(13, 5), sharex=True, sharey=True)
+    gpd.GeoSeries(traj_low["coords"].apply(shapely.wkt.loads)).plot(ax=ax1, color="red", markersize=60, zorder=2);
+    gpd.GeoSeries(traj_mid["coords"].apply(shapely.wkt.loads)).plot(ax=ax2, color="blue", markersize=60, zorder=2);
+    gpd.GeoSeries(traj_high["coords"].apply(shapely.wkt.loads)).plot(ax=ax3, color="green", markersize=60, zorder=2);
+    gpd.GeoSeries(routes_low).plot(ax=ax1, color="grey", linewidth=3, zorder=1, alpha=0.6)
+    gpd.GeoSeries(routes_mid).plot(ax=ax2, color="grey", linewidth=3, zorder=1, alpha=0.6)
+    gpd.GeoSeries(routes_high).plot(ax=ax3, color="grey", linewidth=3, zorder=1, alpha=0.6)
+    ax1.ticklabel_format(useOffset=False)
+    ax2.ticklabel_format(useOffset=False)
+    ax3.ticklabel_format(useOffset=False)
+    ax1.axis("off")
+    ax2.axis("off")
+    ax3.axis("off")
+    ax1.legend(["low"], frameon=False, loc="lower center")
+    ax2.legend(["mid"], frameon=False, loc="lower center")
+    ax3.legend(["high"], frameon=False, loc="lower center")
+    plt.tight_layout()
+    plt.close()
     return fig
 
 def get_day_in_a_life():
